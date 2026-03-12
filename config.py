cbd49661140e958ecded0c5e1349eb79fb1a8223
xuwyx <ezzdurov@gmail.com>
2026-03-13 03:14:24 +0700

    Create config.py with initial configuration
    Added configuration settings for bot and game API.

---
+++ config.py
@@ -0,0 +1,32 @@
+# config.py
+BOT_TOKEN = "8645051590:AAHic0cgu1E12kwEC2g81R0VM9iqf-Sq1PQ"
+GAME_API_TOKEN = "Zluavtkju9WkqLYzGVKg"
+DEFAULT_SENDER_ID = "EfezAdmin1"
+OWNER_ID = 5150403377
+FIREBASE_URL = "https://api-project-7952672729.firebaseio.com"
+API_BASE_URL = "https://api.efezgames.com/v1"
+
+# Файлы данных
+PLAYERS_FILE = "data/players.json"
+INVENTORY_FILE = "data/inventory.json"
+SKINS_MAP_FILE = "айди скинов.txt"
+
+# Модификаторы (качество скинов)
+MODIFIERS_MAP = {
+    10: "Поношенное",
+    12: "Сувенирное Поношенное",
+    14: "StatTrak Поношенное",
+    16: "StatTrak Сувенирное Поношенное",
+    20: "После полевых испытаний",
+    22: "Сувенирное После полевых испытаний",
+    24: "StatTrak После полевых испытаний",
+    26: "StatTrak Сувенирное После полевых испытаний",
+    30: "Немного поношенное",
+    32: "Сувенирное Немного поношенное",
+    34: "StatTrak Немного поношенное",
+    36: "StatTrak Сувенирное Немного поношенное",
+    40: "Прямо с завода",
+    42: "Сувенирное Прямо с завода",
+    44: "StatTrak Прямо с завода",
+    46: "StatTrak Сувенирное Прямо с завода"
+}
